2aa46954d0fe1db43f165c9aa894bd4bc93703b6
Your Name <miguelangel.gonzalez.rz@gmail.com>
2021-05-27 23:51:13 -0400

    Primer ingreso

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Arrays2dimesiones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
