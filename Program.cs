45d2e6f9a781d5bbc876a504b9b9cf028d88bad2
Your Name <miguelangel.gonzalez.rz@gmail.com>
2021-05-28 00:55:48 -0400

    Segunda parte del programa

--- Program.cs
+++ Program.cs
@@ -5,8 +5,24 @@ namespace Arrays2dimesiones
     class Program
     {
         static void Main(string[] args)
+
+            
         {
-            Console.WriteLine("Hello World!");
+
+            const int Filas = 2, Columnas = 5;
+            float[,] Datos = new float[2, 5];
+
+            for (int fila = 0; fila < Filas; fila++)
+            {
+                for (int columna = 0; columna < Columnas; columna++)
+                {
+
+                    Console.WriteLine("Ingrese el dato {0},{1}", fila+1, columna+1);
+                    Datos[fila, columna] = Convert.ToSingle(Console.ReadLine()); 
+                }
+            }
+
+
         }
     }
 }
